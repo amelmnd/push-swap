220cb98a05a850d3858c6d061f82640227148e0a
amelmnd <amelmnd.dev@gmail.com>
2023-08-18 17:08:09 +0200

    push_swap : reprise

--- push_swap/main.c
+++ push_swap/main.c
@@ -6,7 +6,7 @@
 /*   By: amennad <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/04 16:12:00 by amennad           #+#    #+#             */
-/*   Updated: 2023/07/05 13:14:02 by amennad          ###   ########.fr       */
+/*   Updated: 2023/08/18 16:03:17 by amennad          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,18 +15,15 @@
 int	main(void)
 {
 	t_node	*pile_a;
-	t_node	*pile_b;
+	// t_node	*pile_b;
 
-	pile_a = NULL;
-	pile_b = NULL;
+	pile_a->next = NULL;
+	// pile_b = NULL;
 
-	pile_a = push_button_node(pile_a, 2);
-	pile_a = push_top_node(pile_a, 42);
-	pile_a = push_top_node(pile_a, 642);
-	pile_a = push_top_node(pile_a, 8642);
-	if (is_empty(pile_a))
+	ft_push(&pile_a, 1);
+	if (is_empty(&pile_a))
 		printf("empty\n");
 	else
-		print_pile(pile_a);
+		print_pile(&pile_a);
 	return (0);
 }
--- push_swap/utils/fn_utils.c
+++ push_swap/utils/fn_utils.c
@@ -1,25 +1,26 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_libft_utils.c                                   :+:      :+:    :+:   */
+/*   fn_utils.c                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: amennad <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/07/05 10:29:49 by amennad           #+#    #+#             */
-/*   Updated: 2023/07/05 11:05:05 by amennad          ###   ########.fr       */
+/*   Created: 2023/07/05 10:23:49 by amennad           #+#    #+#             */
+/*   Updated: 2023/08/18 15:21:57 by amennad          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
-void	ft_putstr(char *s)
+t_bool	is_empty(t_node *pile)
 {
-	int	i;
+	if (pile == NULL)
+		return (TRUE);
+	return (FALSE);
+}
 
-	i = 0;
-	while (s[i] != '\0')
-	{
-		write(1, &s[i], 1);
-		i++;
-	}
+void	ft_error(void)
+{
+	ft_putstr("Error");
+	exit(EXIT_FAILURE);
 }
